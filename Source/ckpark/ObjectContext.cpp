702733d274897aa8e725486488bd0fd9cb4500c8
chulki-park <klnu86@nate.com>
2017-05-24 22:54:59 +0900

    독시젠 설정파일 추가 오브젝트 컨텍스트 클래스 추가

---
+++ Source/ckpark/ObjectContext.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ckpark.h"
+#include "ObjectContext.h"
+
+ObjectContext::ObjectContext()
+{
+}
+
+ObjectContext::~ObjectContext()
+{
+}
